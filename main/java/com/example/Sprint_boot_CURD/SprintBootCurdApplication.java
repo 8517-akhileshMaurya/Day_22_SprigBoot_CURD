7c5594dc207e890fa1880f0fdd98c9d5c640227d
Akhilesh Maurya <Akhilesh.Maurya@godigit.com>
2024-08-21 12:13:40 +0530

    Main application class

---
+++ main/java/com/example/Sprint_boot_CURD/SprintBootCurdApplication.java
@@ -0,0 +1,13 @@
+package com.example.Sprint_boot_CURD;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SprintBootCurdApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SprintBootCurdApplication.class, args);
+	}
+
+}
